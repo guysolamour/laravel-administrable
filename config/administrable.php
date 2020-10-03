8852e1366e6812855c2cf87c9176b3c07df1d94b
Guy-roland ASSALE <rolandassale@gmail.com>
2020-10-03 16:06:40 +0000

    comments can be answered in back office

--- config/administrable.php
+++ config/administrable.php
@@ -13,7 +13,7 @@ return [
      * Available theme are adminlte,theadmin,cooladmin,tabler,themekit
      * The theme should not be changed once the installation has been done
      */
-    'theme' => 'themekit',
+    'theme' => 'theadmin',
 
     /**
      * The guard used for the installation. By default the value is admin.
